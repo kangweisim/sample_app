347a8e5ff0bc190796031d06725f2f188fc4afc3
kangweisim <kangweisim@gmail.com>
2017-06-01 20:08:13 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token, :activation_token, :reset_token
-	before_save :downcase_email
+  has_many :microposts, dependent: :destroy
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
   before_create :create_activation_digest
-	validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255 },
 										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
-	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 	# Returns the hash digest of the given string.
 	def User.digest(string)
@@ -57,6 +58,10 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
     # Converts email to all lower-case
     def downcase_email
